051d3209e8c46da04e1e630b8e68e54409bbfd66
rudihartono <rudi@radyalabs.com>
2022-03-25 14:22:17 +0700

    Add DI DbContext

--- Trisatech.Kamlinko.WebApp/Program.cs
+++ Trisatech.Kamlinko.WebApp/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Trisatech.Kamlinko.WebApp.Datas;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<KamlinkoDbContext>(
+            dbContextOptions => dbContextOptions
+                .UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")))
+                // The following three options help with debugging, but should
+                // be changed or removed for production.
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+        );
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
